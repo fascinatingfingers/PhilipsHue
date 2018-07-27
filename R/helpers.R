4b858ebb45a739663c9819629baede9cd9007b25
Justin Brantley <fascinatingfingers@icloud.com>
2018-07-27 22:14:40 +0000

    Add function to perform basic error checking and parsing of httr responses

--- R/helpers.R
+++ R/helpers.R
@@ -1,4 +1,51 @@
 
+#' Process HTTP Responses
+#'
+#' This function performs basic error handling and parsing of \code{httr}
+#'   \code{\link[httr]{response}} objects.
+#'
+#' @param x an \code{httr} \code{\link[httr]{response}} object
+#' @param ... parameters passed to \code{\link[jsonlite]{fromJSON}}
+#'
+#' @return Returns the content of the response, parsed into an R-friendly list.
+#'
+process_httr_response <- function(x, ...) {
+    if (!methods::is(x, 'response')) {
+        stop('Input `x` must be an httr response object', call. = FALSE)
+    }
+
+    if (httr::http_type(x) != 'application/json') {
+        stop('API did not return json', call. = FALSE)
+    }
+
+    y <- httr::content(x, 'parsed', encoding = 'UTF-8', ...)
+
+    if (httr::http_error(x)) {
+        stop(
+            sprintf(
+                'API request failed [%s]\n%s',
+                httr::status_code(x),
+                yaml::as.yaml(y)
+            ),
+            call. = FALSE
+        )
+    }
+
+    api_errors <- subset(y, purrr::map_lgl(y, ~ identical(names(.), 'error')))
+
+    if (length(api_errors) > 0) {
+        stop(
+            sprintf(
+                'API request returned errors\n%s',
+                yaml::as.yaml(api_errors)
+            ),
+            call. = FALSE
+        )
+    }
+
+    return(y)
+}
+
 #' Hue Bridge Authentication
 #'
 #' These functions are used primarily for their side effects -- specifically to

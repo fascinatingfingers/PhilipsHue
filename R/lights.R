40dabf5610369cf2a10f2fc0e6d94f5813cf1aaa
Justin Brantley <fascinatingfingers@icloud.com>
2018-07-27 04:02:54 +0000

    Scope `lights` endpoint

---
+++ R/lights.R
@@ -0,0 +1,14 @@
+
+# TODO POST /api/<username>/lights (search for new lights)
+
+# TODO GET /api/<username>/lights/new
+
+# TODO PUT /api/<username>/lights/<id> (rename)
+
+# TODO GET /api/<username>/lights
+
+# TODO GET /api/<username>/lights/<id>
+
+# TODO PUT /api/<username>/lights/<id>/state
+
+# TODO DELETE /api/<username>/lights/<id>

f098902efe053aaac9920b16635035dbf5659525
Justin Brantley <justinbrantley@me.com>
2018-07-28 07:00:46 +0000

    Draft functions for `configuration` endpoints

---
+++ R/configuration.R
@@ -0,0 +1,25 @@
+
+# TODO create_user
+
+# TODO delete_user
+
+# TODO get_configuration
+
+# TODO set_configuration_attributes
+
+#' Get Full State
+#'
+#' @return Returns a list structure with the current state of all devices and
+#'   resources
+#'
+#' @export
+get_state <- function() {
+    state <- process_httr_response(httr::GET(bridge_url()))
+
+    for (scene in names(state$scenes)) {
+        state$scenes[[scene]]$lightstates <- get_scene(scene)$lightstates
+    }
+    rm(scene)
+
+    return(state)
+}

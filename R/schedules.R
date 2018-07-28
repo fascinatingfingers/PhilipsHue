4401a1682a0e0c3fc11a5deb29b60b54e6c391aa
Justin Brantley <justinbrantley@me.com>
2018-07-28 01:36:57 +0000

    Draft functions for `schedules` endpoints

---
+++ R/schedules.R
@@ -0,0 +1,58 @@
+
+#' Hue API: schedules endpoints
+#'
+#' @param id id of a specific schedule, an integer
+#' @param ... named parameters describing schedule attributes
+#'   (e.g. \code{name = 'foo'})
+#'
+#' @return Requests with side effects invisibly return \code{TRUE} upon success.
+#'   GET requests return the response content, parsed into a list.
+#'
+#' @seealso \url{https://developers.meethue.com/documentation/schedules-api-0}
+#'
+#' @name schedules
+
+#' @rdname schedules
+#' @export
+create_schedule <- function(...) {
+    path <- bridge_url('schedules')
+    y <- httr::POST(path, body = list(...), encode = 'json')
+    y <- process_httr_response(y)
+    return(invisible(TRUE))
+}
+
+#' @rdname schedules
+#' @export
+get_schedules <- function() {
+    path <- bridge_url('schedules')
+    y <- httr::GET(path)
+    y <- process_httr_response(y)
+    return(y)
+}
+
+#' @rdname schedules
+#' @export
+get_schedule <- function(id) {
+    path <- bridge_url('schedules', id)
+    y <- httr::GET(path)
+    y <- process_httr_response(y)
+    return(y)
+}
+
+#' @rdname schedules
+#' @export
+set_schedule_attributes <- function(id, ...) {
+    path <- bridge_url('schedules', id)
+    y <- httr::PUT(path, body = list(...), encode = 'json')
+    y <- process_httr_response(y)
+    return(invisible(TRUE))
+}
+
+#' @rdname schedules
+#' @export
+delete_schedule <- function(id) {
+    path <- bridge_url('schedules', id)
+    y <- httr::DELETE(path)
+    y <- process_httr_response(y)
+    return(invisible(TRUE))
+}

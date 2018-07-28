ad31564c97f93fe233afc2153461a27501c8533d
Justin Brantley <justinbrantley@me.com>
2018-07-28 01:13:22 +0000

    Draft functions for `groups` endpoints

---
+++ R/groups.R
@@ -0,0 +1,67 @@
+
+#' Hue API: groups endpoints
+#'
+#' @param id id of a specific group, an integer
+#' @param ... named parameters describing group attributes or state
+#'   (e.g. \code{name = 'foo'}; \code{on = TRUE})
+#'
+#' @return Requests with side effects invisibly return \code{TRUE} upon success.
+#'   GET requests return the response content, parsed into a list.
+#'
+#' @seealso \url{https://developers.meethue.com/documentation/groups-api}
+#'
+#' @name groups
+
+#' @rdname groups
+#' @export
+create_group <- function(...) {
+    path <- bridge_url('groups')
+    y <- httr::POST(path, body = list(...), encode = 'json')
+    y <- process_httr_response(y)
+    return(invisible(TRUE))
+}
+
+#' @rdname groups
+#' @export
+get_groups <- function() {
+    path <- bridge_url('groups')
+    y <- httr::GET(path)
+    y <- process_httr_response(y)
+    return(y)
+}
+
+#' @rdname groups
+#' @export
+get_group <- function(id) {
+    path <- bridge_url('groups', id)
+    y <- httr::GET(path)
+    y <- process_httr_response(y)
+    return(y)
+}
+
+#' @rdname groups
+#' @export
+set_group_attributes <- function(id, ...) {
+    path <- bridge_url('groups', id)
+    y <- httr::PUT(path, body = list(...), encode = 'json')
+    y <- process_httr_response(y)
+    return(invisible(TRUE))
+}
+
+#' @rdname groups
+#' @export
+set_group_state <- function(id, ...) {
+    path <- bridge_url('groups', id, 'action')
+    y <- httr::PUT(path, body = list(...), encode = 'json')
+    y <- process_httr_response(y)
+    return(invisible(TRUE))
+}
+
+#' @rdname groups
+#' @export
+delete_group <- function(id) {
+    path <- bridge_url('groups', id)
+    y <- httr::DELETE(path)
+    y <- process_httr_response(y)
+    return(invisible(TRUE))
+}

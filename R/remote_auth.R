9f9b890be7c7055b361070b74643f434be0f5cf0
Justin Brantley <justinbrantley@me.com>
2019-03-19 04:56:37 +0000

    Add `token` helper

--- R/remote_auth.R
+++ R/remote_auth.R
@@ -37,3 +37,43 @@ remote_auth_request_url <- function(client_id, app_id, bridge_id, device_name, s
         utils::URLencode(state)
     )
 }
+
+#' Request authorization token
+#'
+#' After you obtain an authorization code (see [remote_auth_request_url()]), use
+#' the code to obtain an authorization token.
+#'
+#' @param auth_code authorization code sent to your app's callback URL
+#'   (see [remote_auth_request_url()])
+#' @param client_id your app's client ID (assigned when you registered your app
+#'   with Hue)
+#' @param client_secret your app's client secret (assigned when you registered
+#'   your app with Hue)
+#'
+#' @return If successful, returns a list with authorization/refresh tokens and
+#'   expiration times
+#'
+#' @export
+token <- function(auth_code, client_id, client_secret) {
+    x <- sprintf(
+        'https://api.meethue.com/oauth2/token?code=%s&grant_type=authorization_code',
+        utils::URLencode(auth_code)
+    )
+
+    res <- httr::POST(x, httr::add_headers(
+        Authorization = base64enc::base64encode(charToRaw(paste(client_id, client_secret, sep = ':')))
+    ))
+
+    res_status <- httr::status_code(res)
+
+    res_content <- tryCatch(
+        httr::content(res, as = 'parsed'),
+        error = function(e) {list()}
+    )
+
+    if (res_status %in% 200 && 'access_token' %in% names(res_content)) {
+        return(res_content$access_token)
+    } else {
+        stop('Token request faild with status code: ', res_status, ':\n', yaml::as.yaml(res_content))
+    }
+}
--- tests/testthat/test-remote_auth.R
+++ tests/testthat/test-remote_auth.R
@@ -8,3 +8,20 @@ test_that('helper creates URL to request access to bridge from client', {
         'https://api.meethue.com/oauth2/auth?clientid=client_id&appid=app%20id&deviceid=device_id&devicename=device%20name&state=mock_uuid&response_type=code'
     )
 })
+
+test_that('`token` throws error and prints parsed content if POST fails', {
+    mockery::stub(token, 'httr::POST', list())
+    mockery::stub(token, 'httr::status_code', 'mock status code')
+    mockery::stub(token, 'httr::content', list(errors = c('mock error 1')))
+
+    expect_error(token('mock auth code'), 'mock status code')
+    expect_error(token('mock auth code'), 'mock error 1')
+})
+
+test_that('`token` returns token if POST fails', {
+    mockery::stub(token, 'httr::POST', list())
+    mockery::stub(token, 'httr::status_code', 200)
+    mockery::stub(token, 'httr::content', list(access_token = 'mock access token'))
+
+    expect_identical(token('mock auth code'), 'mock access token')
+})

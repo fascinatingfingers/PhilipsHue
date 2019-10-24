fb340793a850397f47c71586415bdca8405e8e65
Justin Brantley <justinbrantley@me.com>
2019-10-24 06:33:21 +0000

    Consolidate remote auth code

--- R/auth.R
+++ R/auth.R
@@ -32,8 +32,8 @@ auth_local <- function(ip = Sys.getenv('PHILIPS_HUE_BRIDGE_IP'), username = Sys.
     }
 
     # Sanitize, just in case
-    ip <- URLencode(ip)
-    username <- URLencode(username)
+    ip <- utils::URLencode(ip)
+    username <- utils::URLencode(username)
 
     # Save secrets as environment variables
     Sys.setenv(
@@ -43,4 +43,221 @@ auth_local <- function(ip = Sys.getenv('PHILIPS_HUE_BRIDGE_IP'), username = Sys.
 
     # Fin
     return(invisible(TRUE))
+}
+
+
+
+#' Request remote access to user's bridge
+#'
+#' The first step in the remote auth process is to request remote access to the
+#' user's bridge. When they click the generated URL, they will be taken to the
+#' Hue website where they can grant remote access to your app. Once approved,
+#' Hue will redirect them to the callback URL you provided when you registered
+#' your app. This redirect URL will include an authorization code as a query
+#' parameter. Retain this code.
+#'
+#' @param client_id your app's client ID (assigned when you registered your app
+#'   with Hue)
+#' @param app_id your app's ID (the name you registered your app under)
+#' @param bridge_id ID of hue bridge you'd like remote access to
+#' @param bridge_name name of hue bridge you'd like remote access to
+#' @param state arbitrary data that will be included in the redirected response
+#'   (see Details)
+#'
+#' When `state = NULL` (the default), a random hash will be generated. You can
+#' verify this value in the redirected response for an added layer of security.
+#'
+#' @return Returns a URL to request remote access to a user's bridge.
+#'
+#' @examples
+#' authorize_at('client_id', 'app id', 'bridge_id', 'bridge name')
+#'
+#' @export
+authorize_at <- function(
+    client_id = Sys.getenv('PHILIPS_HUE_CLIENT_ID'),
+    app_id = Sys.getenv('PHILIPS_HUE_APP_ID'),
+    bridge_id = Sys.getenv('PHILIPS_HUE_BRIDGE_ID'),
+    bridge_name = Sys.getenv('PHILIPS_HUE_BRIDGE_NAME'),
+    state = NULL
+) {
+    if (is.null(state)) {state <- digest::digest(stats::rnorm(1000))}
+
+    sprintf(
+        'https://api.meethue.com/oauth2/auth?clientid=%s&appid=%s&deviceid=%s&devicename=%s&state=%s&response_type=code',
+        utils::URLencode(client_id),
+        utils::URLencode(app_id),
+        utils::URLencode(bridge_id),
+        utils::URLencode(bridge_name),
+        utils::URLencode(state)
+    )
+}
+
+
+
+#' Request authorization token
+#'
+#' After you obtain an authorization code (see [authorize_at()]), use the code
+#' to obtain an authorization token.
+#'
+#' @param auth_code authorization code sent to your app's callback URL (see
+#'   [authorize_at()])
+#' @param client_id your app's client ID (assigned when you registered your app
+#'   with Hue)
+#' @param client_secret your app's client secret (assigned when you registered
+#'   your app with Hue)
+#'
+#' @return If successful, returns a list with authorization/refresh tokens and
+#'   expiration times
+#'
+#' @export
+request_token <- function(
+    auth_code = Sys.getenv('PHILIPS_HUE_AUTH_CODE'),
+    client_id = Sys.getenv('PHILIPS_HUE_CLIENT_ID'),
+    client_secret = Sys.getenv('PHILIPS_HUE_CLIENT_SECRET')
+) {
+    x <- sprintf(
+        'https://api.meethue.com/oauth2/token?code=%s&grant_type=authorization_code',
+        utils::URLencode(auth_code)
+    )
+
+    res <- httr::POST(x, httr::add_headers(
+        Authorization = base64enc::base64encode(charToRaw(paste(client_id, client_secret, sep = ':')))
+    ))
+
+    res_status <- tryCatch(
+        httr::status_code(res),
+        error = function(e) {NA}
+    )
+
+    res_content <- tryCatch(
+        httr::content(res, as = 'parsed'),
+        error = function(e) {list()}
+    )
+
+    if (res_status %in% 200 && 'access_token' %in% names(res_content)) {
+        return(res_content)
+    } else {
+        stop('Token request faild with status code: ', res_status, ':\n', yaml::as.yaml(res_content))
+    }
+}
+
+
+
+#' Remotely activate a button press on the user's bridge
+#'
+#' After obtaining an authorization token (see [request_token()]), the token can
+#' be used to remotely "press" the button on the Hue bridge as part of a remote
+#' authentication sequence.
+#'
+#' @param token token returned from [request_token()]
+#'
+#' @return Returns `TRUE` (invisibly) if the call was successful.
+#'
+#' @export
+remote_auth <- function(token) {
+    res <- httr::PUT(
+        'https://api.meethue.com/bridge/0/config',
+        httr::add_headers(Authorization = paste('Bearer', token)),
+        body = list(linkbutton = TRUE),
+        encode = 'json'
+    )
+
+    res_status <- tryCatch(
+        httr::status_code(res),
+        error = function(e) {NA}
+    )
+
+    if (res_status %in% 200) {
+        return(invisible(TRUE))
+    } else {
+        res_content <- tryCatch(
+            httr::content(res, as = 'parsed'),
+            error = function(e) {list()}
+        )
+
+        stop('Remote button press faild with status code: ', res_status, ':\n', yaml::as.yaml(res_content))
+    }
+}
+
+#' Whitelist your app and receive a username
+#'
+#' @param token token returned from [request_token()]
+#' @param app_id your app's ID (the name you registered your app under)
+#'
+#' @return Adds your app ID to the list of whitelisted apps on the user's bridge
+#'   and returns a username that can be used to authenticate future requests.
+#'
+#' @export
+request_app_username <- function(token, app_id = Sys.getenv('PHILIPS_HUE_APP_ID')) {
+    res <- httr::POST(
+        'https://api.meethue.com/bridge',
+        httr::add_headers(Authorization = paste('Bearer', token)),
+        body = list(devicetype = app_id),
+        encode = 'json'
+    )
+
+    res_status <- tryCatch(
+        httr::status_code(res),
+        error = function(e) {NA}
+    )
+
+    res_content <- tryCatch(
+        httr::content(res, as = 'parsed'),
+        error = function(e) {list()}
+    )
+
+    if (res_status %in% 200 && 'success' %in% names(res_content[[1]])) {
+        return(res_content[[1]]$success$username)
+    } else {
+        stop('Token request faild with status code: ', res_status, ':\n', yaml::as.yaml(res_content))
+    }
+}
+
+
+
+#' Refresh access token
+#'
+#' Tokens received by calling [request_token()] expire but can be refreshed by
+#' a simple call to [refresh_token()].
+#'
+#' @param refresh_token refresh token returned from [request_token()] (or
+#'   a subsequent [refresh_token()])
+#' @param client_id your app's client ID (assigned when you registered your app
+#'   with Hue)
+#' @param client_secret your app's client secret (assigned when you registered
+#'   your app with Hue)
+#'
+#' @return If successful, returns a list with authorization/refresh tokens and
+#'   expiration times
+#'
+#' @export
+refresh_token <- function(
+    refresh_token,
+    client_id = Sys.getenv('PHILIPS_HUE_CLIENT_ID'),
+    client_secret = Sys.getenv('PHILIPS_HUE_CLIENT_SECRET')
+) {
+    res <- httr::POST(
+        'https://api.meethue.com/oauth2/refresh?grant_type=refresh_token',
+        httr::add_headers(
+            Authorization = base64enc::base64encode(charToRaw(paste(client_id, client_secret, sep = ':')))
+        ),
+        body = list(refresh_token = refresh_token),
+        encode = 'form'
+    )
+
+    res_status <- tryCatch(
+        httr::status_code(res),
+        error = function(e) {NA}
+    )
+
+    res_content <- tryCatch(
+        httr::content(res, as = 'parsed'),
+        error = function(e) {list()}
+    )
+
+    if (res_status %in% 200) {
+        return(res_content)
+    } else {
+        stop('Token refresh faild with status code: ', res_status, ':\n', yaml::as.yaml(res_content))
+    }
 }
--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -1,5 +1,5 @@
 
-context('Auth-local')
+context('auth_local')
 
 MOCK_CREDS_LOCAL <- c(
     PHILIPS_HUE_BRIDGE_IP = '0.0.0.0',
@@ -29,4 +29,99 @@ test_that('TRUE is returned upon success', {
     withr::with_envvar(MOCK_CREDS_LOCAL, {
         expect_true(auth_local())
     })
+})
+
+
+
+context('authorize_at')
+
+test_that('helper creates URL to request access to bridge from client', {
+    mockery::stub(authorize_at, 'digest::digest', 'mock_hash')
+    expect_identical(
+        authorize_at('client_id', 'app id', 'device_id', 'device name'),
+        'https://api.meethue.com/oauth2/auth?clientid=client_id&appid=app%20id&deviceid=device_id&devicename=device%20name&state=mock_hash&response_type=code'
+    )
+})
+
+
+
+context('request_token')
+
+test_that('`request_token` throws error and prints parsed content if POST fails', {
+    mockery::stub(request_token, 'httr::POST', list())
+    mockery::stub(request_token, 'httr::status_code', 'mock status code')
+    mockery::stub(request_token, 'httr::content', list(errors = c('mock error 1')))
+
+    expect_error(request_token('mock auth code'), 'mock status code')
+    expect_error(request_token('mock auth code'), 'mock error 1')
+})
+
+test_that('`request_token` returns token if POST succeeds', {
+    mockery::stub(request_token, 'httr::POST', list())
+    mockery::stub(request_token, 'httr::status_code', 200)
+    mockery::stub(request_token, 'httr::content', list(access_token = 'mock access token'))
+
+    expect_identical(request_token('mock auth code'), list(access_token = 'mock access token'))
+})
+
+
+
+context('remote_auth')
+
+test_that('`remote_auth` throws error and prints parsed content if POST fails', {
+    mockery::stub(remote_auth, 'httr::PUT', list())
+    mockery::stub(remote_auth, 'httr::status_code', 'mock status code')
+    mockery::stub(remote_auth, 'httr::content', list(errors = c('mock error 1')))
+
+    expect_error(remote_auth('mock access token'), 'mock status code')
+    expect_error(remote_auth('mock access token'), 'mock error 1')
+})
+
+test_that('`remote_auth` returns TRUE if POST succeeds', {
+    mockery::stub(remote_auth, 'httr::POST', list())
+    mockery::stub(remote_auth, 'httr::status_code', 200)
+
+    expect_true(remote_auth('mock access token'))
+})
+
+
+
+context('request_app_username')
+
+test_that('`request_app_username` throws error and prints parsed content if POST fails', {
+    mockery::stub(request_app_username, 'httr::POST', list())
+    mockery::stub(request_app_username, 'httr::status_code', 'mock status code')
+    mockery::stub(request_app_username, 'httr::content', list(errors = c('mock error 1')))
+
+    expect_error(request_app_username('mock access token', 'mock_app_id'), 'mock status code')
+    expect_error(request_app_username('mock access token', 'mock_app_id'), 'mock error 1')
+})
+
+test_that('`request_app_username` returns username if POST succeeds', {
+    mockery::stub(request_app_username, 'httr::POST', list())
+    mockery::stub(request_app_username, 'httr::status_code', 200)
+    mockery::stub(request_app_username, 'httr::content', list(list(success = list(username = 'mock username'))))
+
+    expect_identical(request_app_username('mock access token', 'mock_app_id'), 'mock username')
+})
+
+
+
+context('refresh_token')
+
+test_that('`refresh_token` throws error and prints parsed content if POST fails', {
+    mockery::stub(refresh_token, 'httr::POST', list())
+    mockery::stub(refresh_token, 'httr::status_code', 'mock status code')
+    mockery::stub(refresh_token, 'httr::content', list(errors = c('mock error 1')))
+
+    expect_error(refresh_token('mock refresh token', 'mock_app_id'), 'mock status code')
+    expect_error(refresh_token('mock refresh token', 'mock_app_id'), 'mock error 1')
+})
+
+test_that('`refresh_token` returns token if POST succeeds', {
+    mockery::stub(refresh_token, 'httr::POST', list())
+    mockery::stub(refresh_token, 'httr::status_code', 200)
+    mockery::stub(refresh_token, 'httr::content', list(access_token = 'mock access token'))
+
+    expect_identical(refresh_token('mock refresh code'), list(access_token = 'mock access token'))
 })
